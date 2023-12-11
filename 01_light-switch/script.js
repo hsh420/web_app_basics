006a7d91290e76fd59b911e228008343c460809a
Hendrik Schwind-Hansen <info@hendrik-sh.de>
2023-12-11 16:23:21 +0100

    initial commit + solved 1st task

---
+++ 01_light-switch/script.js
@@ -0,0 +1,16 @@
+const body = document.querySelector(".body");
+const btn = document.querySelector(".toggle-btn");
+
+function toggleLight() {
+  if (body.className === "body") {
+    body.className += " dark";
+    btn.className += " lightenedBorder";
+  } else if (body.className === "body dark") {
+    body.className = "body light";
+    btn.className = "toggle-btn";
+  } else {
+    body.className = "body dark";
+    btn.className += " lightenedBorder";
+  }
+  console.log(body);
+}
