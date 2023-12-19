5577ace4d9dee87cca6399de5676c69fcc518051
Hendrik Schwind-Hansen <info@hendrik-sh.de>
2023-12-19 16:57:21 +0100

    fix: changed input-accent-color to fit the design; ref: added comments on script.js and changed structure for better understanding

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -1,7 +1,16 @@
+// Initialwerte
 let data = [];
 let selected = "all";
 
+// Dokumentenelemente
+const select = document.querySelector("#selectContainer");
+const list = document.querySelector("#list");
+const input = document.querySelector("#add");
+const addBtn = document.querySelector("#addBtn");
+
+// Initiale Logiken: nach Daten suchen, diese Daten anzeigen (falls keine vorhanden: Platzhalter)
 checkForData();
+renderToDos(data);
 
 function checkForData() {
   if (localStorage.getItem("data") === null || undefined) {
@@ -12,21 +21,17 @@ function checkForData() {
   }
 }
 
-const select = document.querySelector("#selectContainer");
-const list = document.querySelector("#list");
-const input = document.querySelector("#add");
-const addBtn = document.querySelector("#addBtn");
-
-renderToDos(data);
-
 function renderToDos(data) {
+  // Liste leeren
   list.innerHTML = "";
   if (data === undefined || data.length === 0) {
+    // Platzhalter für Liste, falls keine Daten vorhanden sind
     const li = document.createElement("li");
     const placeholder = document.createTextNode("Noch keine To-Dos...");
     li.appendChild(placeholder);
     list.appendChild(li);
   } else {
+    // dynamische Erstellung der Liste mit den Daten
     data.forEach((element) => {
       const li = document.createElement("li");
       const text = document.createTextNode(element.description);
@@ -43,6 +48,7 @@ function renderToDos(data) {
   }
 }
 
+// Filter-Logiken, inklusive State-Management bzgl. des aktuell ausgewählten Filters
 select.addEventListener("input", (event) => {
   event.preventDefault();
   const value = event.target.value;
@@ -80,7 +86,9 @@ function filterOpenToDos() {
   renderToDos(filteredData);
 }
 
+// To-Dos hinzufügen
 addBtn.addEventListener("click", (event) => {
+  // Validation: To-Do existiert bereits oder input-field leer?
   for (let i = 0; i < data.length; i++) {
     if (data[i].description.toLowerCase() === input.value.toLowerCase()) {
       window.alert("To Do existiert bereits!");
@@ -91,6 +99,7 @@ addBtn.addEventListener("click", (event) => {
     window.alert("Beschreibung fehlt!");
     return;
   }
+  // wenn Validierung erfolgreich abgeschlossen, Erstellung eines neuen To-Dos + input zurücksetzen + Daten speichern und neu rendern
   const newToDo = {
     description: input.value,
     id: new Date().getTime(),
@@ -102,6 +111,7 @@ addBtn.addEventListener("click", (event) => {
   renderToDos(data);
 });
 
+// Status der To-Dos ändern und Änderungen erfassen; Daten jeweils aktuell laden
 list.addEventListener("change", (event) => {
   data.forEach((element) => {
     if (
@@ -117,6 +127,7 @@ list.addEventListener("change", (event) => {
     }
   });
   localStorage.setItem("data", JSON.stringify(data));
+  // Daten neu rendern ist notwendig, wenn angewendeten Filtern Daten geändert werden
   if (selected === "done") {
     filterDoneToDos();
   } else if (selected === "open") {
@@ -126,6 +137,7 @@ list.addEventListener("change", (event) => {
   }
 });
 
+// als erledigt markierte To-Dos endgültig entfernen mit vorheriger Sicherheitsabfrage
 function resetDone() {
   if (window.confirm("Erledigte To-Dos wirklich löschen?")) {
     const newData = data.filter((element) => {
